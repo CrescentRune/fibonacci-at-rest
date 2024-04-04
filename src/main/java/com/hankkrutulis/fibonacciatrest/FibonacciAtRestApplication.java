d937d1a932dbdc4dce4b46b6d5fd0a2ccb95e52a
Hankus <henrykrutulis@gmail.com>
2024-04-04 17:09:05 -0400

    Initialized git repo from Spring start

---
+++ src/main/java/com/hankkrutulis/fibonacciatrest/FibonacciAtRestApplication.java
@@ -0,0 +1,13 @@
+package com.hankkrutulis.fibonacciatrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibonacciAtRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FibonacciAtRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hankkrutulis/fibonacciatrest/FibonacciAtRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hankkrutulis.fibonacciatrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibonacciAtRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
