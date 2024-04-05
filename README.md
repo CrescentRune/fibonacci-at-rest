224095bb59f89a09ac0a57b7730ecb2c02c402c6
Hankus <henrykrutulis@gmail.com>
2024-04-05 01:26:50 -0400

    Updated README and added basic Controller unit tests

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 Assumptions:
 - State is shared between multiple potential clients
 - Since state is shared, rather than throwing an error on prev() when the list is at the beginning, it returns zero
+- The equivalent happens at the upperbound for longs.
 
 Design Notes:
-- I attempted something somewhat canonical for Spring, but in a practical application, this lacks any persistence
+- I attempted something somewhat canonical for Spring, so most of the logic is in the service layer.
 - Base case prepopulates for items 0,1, which makes the tabulation for the rest of the fibonacci items better
-- I started out with ints, but was unsatisfied with how few reloads of '/next' it took to reach the upper limit. I then planned out a whole solution which would switch to BigInts in the array, but only for the items that needed it to save on memory, but I decided that would look horrible. I decided using Longs instead gave a sufficiently satisfying upper limit.
+- It occurred to me to use `BigInt`, since the upper bound for fibonacci numbers in `long` form is not _that_ large, however I thought `long` had a high enough ceiling to be satisfying, faster, easier to test, while also emulating the fact that a production server should have some reasonable limit rather than extending to an unreasonable weight on server capacity.
+- I also considered using a 302 redirect on an error for '/previous' and 'next' however since this uses a REST Controller, it seemed better not to.
---
+++ src/test/java/com/hankkrutulis/fibonacciatrest/web/TestFibonacciController.java
@@ -0,0 +1,67 @@
+package com.hankkrutulis.fibonacciatrest.web;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.annotation.Description;
+
+import com.hankkrutulis.fibonacciatrest.service.FibonacciService;
+
+
+/*
+ * Since I implemented a service layer for the main logic, the endpoints are
+ * just pass-through. I added these simple tests for coverage and extensibility
+ * nonetheless.
+ */
+@ExtendWith(MockitoExtension.class)
+public class TestFibonacciController {
+ 
+  @InjectMocks
+  FibonacciController fibonacciController;
+
+  @Mock
+  FibonacciService fibonacciService;
+ 
+
+  @Test
+  void fibServiceExists() {
+    assertNotNull(this.fibonacciController);
+    assertNotNull(this.fibonacciService);
+  }
+
+  @Test
+  @Description("/get -- value should pass through")
+  void getReturnsResult() {
+    Mockito.when(fibonacciService.getCurrentItem()).thenReturn(13L);
+    String response = fibonacciController.getCurrent();
+
+    assertEquals("13", response);
+  }
+
+  @Test
+  @Description("/prev -- value should pass through")
+  void prevReturnsResult() {
+    Mockito.when(fibonacciService.getPreviousItem()).thenReturn(8L);
+    String response = fibonacciController.getPrevious();
+
+    assertEquals("8", response);
+  }
+
+
+  @Test
+  @Description("/next -- value should pass through")
+  void nextReturnsResult() {
+    Mockito.when(fibonacciService.getNextItem()).thenReturn(13L);
+    String response = fibonacciController.getNext();
+
+    assertEquals("13", response);
+  }
+
+  
+}
