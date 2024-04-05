25914c42776230fd2ccd576a592214f01492b0c5
Hankus <henrykrutulis@gmail.com>
2024-04-05 01:33:19 -0400

    added redirect to /get from root

--- src/main/java/com/hankkrutulis/fibonacciatrest/web/FibonacciController.java
+++ src/main/java/com/hankkrutulis/fibonacciatrest/web/FibonacciController.java
@@ -1,16 +1,25 @@
 package com.hankkrutulis.fibonacciatrest.web;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hankkrutulis.fibonacciatrest.service.FibonacciService;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 @RestController
 public class FibonacciController {
 
     @Autowired
     private FibonacciService fibonacciService;
+
+    @GetMapping(value = "/")
+    public void getRoot(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/get");
+    }
 
     @GetMapping(value = "/get")
     public String getCurrent() {
